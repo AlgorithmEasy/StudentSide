02d232bf066c743523f3e588f484d4953dd8f9bd
SmallY <git@smallyy.cn>
2021-12-11 14:36:48 +0800

    修改 UnAuthorized 提示

--- Shared/ErrorMessages.cs
+++ Shared/ErrorMessages.cs
@@ -3,7 +3,7 @@
     public static class ErrorMessages
     {
         public const string ConnectErrorMessage = "未能与服务器建立连接，请检查网络后重试。";
-        public const string UnAuthorizedErrorMessage = "登录失效，请重新登录。";
+        public const string UnAuthorizedErrorMessage = "登录失效或权限不足，请重新登录。";
         public const string ResponseErrorMessage = "从服务器获取数据失败，请检查网络后重试。";
     }
 
